90ee5825313b085f8eb8482fb56ac8249b60529d
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2025-01-12 03:11:37 +0200

    remove body-parser and clean the server file

--- starter_project/src/server/index.js
+++ starter_project/src/server/index.js
@@ -1,33 +1,25 @@
-var path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
 dotenv.config();
 
 const app = express();
 
-const cors = require('cors');
-
 app.use(cors());
-app.use(bodyParser.json());
-
-console.log(__dirname);
+app.use(express.json());
 
 // Variables for url and api key
 
-
-app.get('/', function (req, res) {
-    res.send("This is the server API page, you may access its services via the client app.");
+app.get("/", function (req, res) {
+  res.send(
+    "This is the server API page, you may access its services via the client app."
+  );
 });
-
 
 // POST Route
 
-
-
 // Designates what port the app will listen to for incoming requests
 app.listen(8000, function () {
-    console.log('Example app listening on port 8000!');
+  console.log("Example app listening on port 8000!");
 });
-
-
